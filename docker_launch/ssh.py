78d2c9a486f8522f3816d7d8877180d1fea02be3
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-08-06 12:14:17 +0900

    #4 Fix overlooked argument

--- docker_launch/ssh.py
+++ docker_launch/ssh.py
@@ -3,7 +3,28 @@ from typing import Tuple
 import paramiko
 
 
-def parse_address(address: str, username: str = None) -> Tuple[str, str]:
+def _parse_address(address: str, username: str = None) -> Tuple[str, str]:
+    """Parse IP address, either in format user@ipaddr or separately provided.
+
+    Parameters
+    ----------
+    address
+        IP address or username@ipaddr.
+    username
+        User name.
+
+    Returns
+    -------
+    tuple[str, str]
+        Parsed (IP address, user name).
+
+    Raises
+    ------
+    ValueError
+        If address is given in username@ipaddr format but inconsistent username is given
+        as separate argument.
+
+    """
     _username, ipaddr = username, address
     if address.find("@") != -1:
         _username, ipaddr = address.split("@", 1)
@@ -16,9 +37,9 @@ def parse_address(address: str, username: str = None) -> Tuple[str, str]:
 def check_key_ssh(address: str, *, username: str = None, port: int = 22) -> bool:
     client = paramiko.SSHClient()
     client.load_system_host_keys()
-    ipaddr, username = parse_address(address, username)
+    ipaddr, username = _parse_address(address, username)
     try:
-        client.connect(ipaddr, username=username)
+        client.connect(ipaddr, username=username, port=port)
         client.close()
         return True
     except paramiko.AuthenticationException:
